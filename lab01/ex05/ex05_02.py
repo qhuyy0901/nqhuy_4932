3ad23ec08eca5f41770db7e6c926fc93b21b5775
Nguyen Quang Huy <qhuyy0901@gmail.com>
2026-05-16 13:09:51 +0700

    lab01

--- lab01/ex05/ex05_02.py
+++ lab01/ex05/ex05_02.py
@@ -1,12 +1,12 @@
 import re
 
-# Chuỗi đầu vào
+
 s = "-100#^sdfkj8902w3ir021@swf-20"
 
 # Tìm tất cả số nguyên (dương và âm)
 numbers = [int(num) for num in re.findall(r'-?\d+', s)]
 
-# Tính tổng số dương và âm
+
 positive_sum = sum(num for num in numbers if num > 0)
 negative_sum = sum(num for num in numbers if num < 0)
 
